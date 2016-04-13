974fde444c053255eeec11fe3a830ad1acc77c89
Marsanmar19 <mdsmartin@hotmail.com>
2016-04-13 19:33:44 +0200

    Initial commit

---
+++ app/src/main/java/com/app/droneuse/droneuseapp/FPVTutorialActivity.java
@@ -0,0 +1,13 @@
+package com.app.droneuse.droneuseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FPVTutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fpvtutorial);
+    }
+}
